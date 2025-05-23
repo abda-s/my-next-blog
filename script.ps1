ae0ff9e0dcab84f63056410059e3eb7df3e58f02
abdullah salameh <abdullahsalameh66@gmail.com>
2025-05-23 21:19:31 +0300

    Deploy to gh-pages - 5/23/2025 9:19:31 PM

--- script.ps1
+++ script.ps1
@@ -9,9 +9,9 @@ if ($branchExists) {
 
 # Perform subtree split
 Write-Host "Running npm scripts..."
-# npm run copy-posts-from-obsidian
-# npm run process-images
-# npm run build
+npm run copy-posts-from-obsidian
+npm run process-images
+npm run build
 try {
 git add .
 git commit -m "Deploy to gh-pages - $(Get-Date -Format G)"
