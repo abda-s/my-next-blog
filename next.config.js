5375fbc81e532f5ad988d1fa0f17e4d38b9f81b9
abdullah salameh <abdullahsalameh66@gmail.com>
2025-05-25 20:55:20 +0300

    Deploy to gh-pages - 5/25/2025 8:55:20 PM

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const nextConfig = {
   output: 'export', // Enable static HTML export
-  basePath: '/my-next-blog', // Replace with your GitHub repo name if deploying under a subpath
-  assetPrefix: '/my-next-blog',
+  basePath: '', // Replace with your GitHub repo name if deploying under a subpath
+  assetPrefix: '',
   trailingSlash: true, // Required for GitHub Pages to handle static routes
 };
 
