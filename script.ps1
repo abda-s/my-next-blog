874cf45af983eaf82c512f0d0d78ceded651d59e
abdullah salameh <abdullahsalameh66@gmail.com>
2025-05-25 19:16:04 +0300

    Deploy to gh-pages - 5/25/2025 7:16:04 PM

--- script.ps1
+++ script.ps1
@@ -9,11 +9,19 @@ if ($branchExists) {
     git branch -D gh-pages-deploy
 }
 
+
+
+Write-Host "/-/-/--/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/--/-/-/-/-/-/-/--/"
 # Perform subtree split
 Write-Host "Running npm scripts..."
 npm run copy-posts-from-obsidian
 npm run process-images
 npm run build
+
+Write-Host "Finished npm scripts..."
+Write-Host "/-/-/--/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/--/-/-/-/-/-/-/--/"
+Write-Host "Committing and pushing changes to main branch..."
+
 try {
 git add .
 git commit -m "Deploy to gh-pages - $(Get-Date -Format G)"
@@ -34,5 +42,11 @@ try {
 
 # Delete the temporary branch
 git branch -D gh-pages-deploy
-
+Write-Host "/-/-/--/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/--/-/-/-/-/-/-/--/"
+# Add and commit changes to the main branch
+Write-Host "Committing and pushing changes to main branch..."
+git add .
+git commit -m "Update blog content and build output - $(Get-Date -Format G)"
+git push origin main
+Write-Host "/-/-/--/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/--/-/-/-/-/-/-/--/"
 Write-Host "All done! Site synced, processed, committed, built, and deployed."
